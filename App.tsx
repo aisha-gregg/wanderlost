140f526629f19531923a850ae657e55e609ef5e1
Aisha Gregg <aishagregg@gmail.com>
2020-06-25 20:49:33 +0200

    create form

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import logo from "./assets/wanderlostlogo.svg";
 
 export default function App() {
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
+      <TextInput
+        style={{ height: 60 }}
+        placeholder="Sign in with Google"
+        onChangeText={(text) => setText(text)}
+        defaultValue={text}
+      ></TextInput>
+
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
@@ -12,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
