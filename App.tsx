9d22e6d7a8390bcf5b77ed7ef6806be4f166f2cb
Aisha Gregg <aishagregg@gmail.com>
2020-06-30 00:02:55 +0200

    Include svg

--- App.tsx
+++ App.tsx
@@ -1,36 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "react-native-gesture-handler";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Login } from "./pages/Login";
 import { createStackNavigator } from "@react-navigation/stack";
-
-import logo from "./assets/wanderlostlogo.svg";
 import { NavigationContainer } from "@react-navigation/native";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-function Login() {
-  const [text, setText] = useState("");
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={{ height: 60 }}
-        placeholder="Sign in with Google"
-        onChangeText={(text) => setText(text)}
-        defaultValue={text}
-      ></TextInput>
-
-      <Text>WanderLost</Text>
-      <Text>Discover Wonders</Text>
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,20 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import "react-native-gesture-handler";
-const { Navigation } = require("react-native-navigation");
-import { createStackNavigator } from "@react-navigation/stack";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Button } from "react-native-elements";
+import { StyleSheet, Text, View } from "react-native";
+import Logo from "./logo.svg";
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+export function Login() {
   return (
-    Navigation.push(props.componentId, {
-        component: {
-          name: 'Settings', // Push the screen registered with the 'Settings' key
-          options: { // Optional options object to configure the screen
-            topBar: {
-              title: {
-                text: 'Settings' // Set the TopBar title of the new Screen
-              }
-            }
-          }
-        }
-      });
+    <View style={styles.container}>
+      <Logo></Logo>
+      <Text style={{ height: 200 }}>WanderLost</Text>
+      <Text style={{ height: 200 }}>Discover Wonders</Text>
+      <Button title="Sign in with Google" type="outline" />
+    </View>
+  );
+}
